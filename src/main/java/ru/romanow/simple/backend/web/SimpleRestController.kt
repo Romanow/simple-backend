8c21dc72f781f236f93d1d3f53538b88803ffea0
aromanov <aromanov@inno.tech>
2022-12-04 01:43:12 +0400

    fix CORS

--- src/main/java/ru/romanow/simple/backend/web/SimpleRestController.kt
+++ src/main/java/ru/romanow/simple/backend/web/SimpleRestController.kt
@@ -1,15 +1,16 @@
 package ru.romanow.simple.backend.web
 
 import org.springframework.data.domain.Example
-import org.springframework.data.domain.ExampleMatcher
 import org.springframework.data.domain.ExampleMatcher.matching
 import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import ru.romanow.simple.backend.domain.GreetingData
 import ru.romanow.simple.backend.repository.GreetingDataRepository
 
+@CrossOrigin
 @RestController
 class SimpleRestController(
     private val greetingDataRepository: GreetingDataRepository
